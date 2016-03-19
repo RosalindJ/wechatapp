2ea42638f3651eaded47808fee1ecec6420db7ec
TangMeirong <1135214659@qq.com>
2016-03-19 15:00:17 +0800

    revise fl.py

--- app/wechat/wxapi/backends/fl.py
+++ app/wechat/wxapi/backends/fl.py
@@ -85,28 +85,29 @@ def ans_base(callback=None):
     def wrap(func):
         @wraps(func)
         def inner(*args, **kwargs):
-            openid = request.cookies.get('openid')
-            userinfo = None
-            if not openid:
+            # openid = request.cookies.get('openid')
+            # userinfo = None
+            # if not openid:
                 code = request.args.get("code")
                 if not code:
                     return redirect(WeixinHelper.oauth2_base(request.url))
-                else:
-                    data = json.loads(WeixinHelper.getAccessTokenByCode(code))
-                    openid = data["openid"]
-                    # userinfo = json.loads(WeixinHelper.getSnsapiUserInfo(
-                    #     access_token, openid))
-                    userinfo = {"status":"yes"}
-            else:
-                ok, openid = Helper.check_cookie(openid)
-                if not ok:
-                    return redirect("/")
-            g.openid = openid
-            if callable(callback):
-                # status = "yes"
-                g.user = callback(openid, userinfo)
-            response = func()
-            return response
+            #     else:
+            #         data = json.loads(WeixinHelper.getAccessTokenByCode(code))
+            #         openid = data["openid"]
+            #         userinfo = json.loads(WeixinHelper.getSnsapiUserInfo(
+            #             access_token, openid))
+            #         # userinfo = {"status":"yes"}
+            # else:
+            #     ok, openid = Helper.check_cookie(openid)
+            #     if not ok:
+            #         return redirect("/")
+            # g.openid = openid
+            # if callable(callback):
+            #     # status = "yes"
+            #     g.user = callback(openid, userinfo)
+            # response = func()
+            # return response
+            return func()
 
         return inner
 

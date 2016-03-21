75f4e3373581ec4d2c295630b1a667e81dcd9b4f
TangMeirong <1135214659@qq.com>
2016-03-21 16:55:04 +0800

    revise ordering.js

--- app/static/app/js/ordering.js
+++ app/static/app/js/ordering.js
@@ -36,6 +36,7 @@
 						data:"netId="+$("#pro_name").attr("data-netid")+"&price="+$("#price").html()+"&quantity="+$("#num").val()+"&sendWay="+$("#sendWay").html()+"&name="+$("#name").val()+"&tel="+$("#tel").val()+"&address="+$("#street").val()+"&message="+$("#message").val()+"&amount="+$("#amount").html(),
 						success:function(data){
 							console.log(data);
+							data = JSON.parse(data);
 							console.log(data["timeStamp"]);
 							console.log(data["nonceStr"]);
 							console.log(data["package"]);

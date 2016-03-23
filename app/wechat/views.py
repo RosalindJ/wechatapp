1a9f941904aec1c941652fdd40b3127c64c8e9fb
TangMeirong <1135214659@qq.com>
2016-03-23 18:24:01 +0800

    revise views.py

--- app/wechat/views.py
+++ app/wechat/views.py
@@ -156,7 +156,7 @@ def notify_url():
     xml = request.form #$GLOBALS['HTTP_RAW_POST_DATA']
     notify.saveData(xml)
 
-     #验证签名，并回应微信
+    #验证签名，并回应微信
     #对后台通知交互时，如果微信收到商户的应答不是成功或超时，微信认为通知失败，
     #微信会通过一定的策略（如30分钟共8次）定期重新发起通知
     #尽可能提高通知的成功率，但微信不保证通知最终能成功。
@@ -188,7 +188,7 @@ def notify_url():
         #例如：数据库操作
         #例如：推送支付完成信息
 
-    return "notify_url"
+    return ""
 
 
 
